dd6dd86f1b7b2a477154697572c080a99cc655b9
teddyyGO <geonexusrp@gmail.com>
2025-12-05 13:05:23 +0100

    Added API + Search

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { HomePage } from "./pages/HomePage";
+import { SearchPage } from "./pages/SearchPage";
+import { MovieDetailPage } from "./pages/MovieDetailPage";
+import { ProfilePage } from "./pages/ProfilePage";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/movie/:id" element={<MovieDetailPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
